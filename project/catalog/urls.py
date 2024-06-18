de09e37f783916753aec8c7ca48d82453a0726df
JoinCastl <159475451+JoinCastl@users.noreply.github.com>
2024-06-18 17:48:30 +0300

    Update urls.py

--- project/catalog/urls.py
+++ project/catalog/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from . import views
+import django.urls
+from catalog import views
 
 urlpatterns = [
 
-    path('', views.index, name='index'),
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
+    django.urls.path('', views.index, name='index'),
+    django.urls.path('', views.home, name='home'),
+    django.urls.path('contact/', views.contact, name='contact'),
+    django.urls.path('', views.home, name='home'),
+    django.urls.path('contact/', views.contact, name='contact'),
 
 ]
